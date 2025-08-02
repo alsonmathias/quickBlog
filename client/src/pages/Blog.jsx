01baaa4705bcd9f6271f30d0f10663c50b58348b
alsonmathias <alsonmathias1209@gmail.com>
2025-08-02 20:52:14 +0530

    Added add blog into database

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,11 +3,15 @@ import { useParams } from "react-router-dom";
 import { assets, blog_data, comments_data } from "../assets/assets";
 import Navbar from "../components/Navbar";
 import Moment from "moment";
-import Footer from '../components/Footer'
+import Footer from "../components/Footer";
 import Loader from "../components/Loader";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const Blog = () => {
   const { id } = useParams();
+
+  const { axios } = useAppContext();
 
   const [data, setData] = useState(null);
 
@@ -16,14 +20,43 @@ const Blog = () => {
   const [content, setContent] = useState("");
 
   const fetcBlogData = async () => {
-    const data = blog_data.find((item) => item._id === id);
-    setData(data);
+    try {
+      const { data } = await axios.get(`/api/blog/${id}`);
+      data.success ? setData(data.blog) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
   const addComment = async (e) => {
     e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/blog/add-comment", {
+        blog: id,
+        name,
+        content,
+      });
+      if (data.success) {
+        toast.success(data.message);
+        setName("");
+        setContent("");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
   const fetchCommetns = async () => {
-    setComments(comments_data);
+    try {
+      const { data } = await axios.post("/api/blog/comments", { blogId: id });
+      if (data.success) {
+        setComments(data.comments);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
   useEffect(() => {
     fetcBlogData();
@@ -131,10 +164,10 @@ const Blog = () => {
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   ) : (
-    <Loader/>
+    <Loader />
   );
 };
 
